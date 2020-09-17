23444034894f1897d6180fdcc72554631f7edd1b
asoosoft <asoosoft7@gmail.com>
2020-09-17 23:25:27 +0900

    aa

--- Network/TestServer/routes/users.js
+++ Network/TestServer/routes/users.js
@@ -4,7 +4,27 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) 
 {
-	res.send('respond with a resource');
+    console.log(req.query.name);
+   
+    //일반 ajax 요청일 경우
+    if(req.query.name)
+    {
+        res.send('Hello~ ' + req.query.name);
+    }
+    
+    //setData 일 경우
+    else
+    {
+        var resData = 
+        {
+            email: 'test@gmail.com',
+            age: 20
+        };
+    
+        //json 형식의 데이터
+        res.json(resData);   
+        res.end();
+    }
 });
 
 
@@ -35,9 +55,35 @@ router.post('/', function(req, res, next)
 
 	console.log(data.body.InBlock1);
 
-	var resData;
+	var resData = null;
 
-	if(data.header.query_name=='ac002')
+    if(data.header.query_name=='ac001')
+	{
+		resData = 
+		{
+			//클라이언트에서 보낸 헤더값을 그대로 다시 응답데이터에 셋팅한다.
+			header: 
+			{
+				packet_id: data.header.packet_id,
+				query_name: data.header.query_name,
+				error_code: 1000
+			}, 
+			body: 
+			{
+				//이곳은 전문에 따라 다르게 응답값을 셋팅해 준다.
+				OutBlock1: 
+				[
+					{
+						'nick_name': '아수쿨',
+						'uid': 1234567,
+						'email': 'asoocool@gmail.com'
+					}
+				]
+			} 
+		};
+	}
+
+	else if(data.header.query_name=='ac002')
 	{
 		resData = 
 		{
@@ -68,32 +114,7 @@ router.post('/', function(req, res, next)
 			});
 		}
 	}
-	else
-	{
-		resData = 
-		{
-			//클라이언트에서 보낸 헤더값을 그대로 다시 응답데이터에 셋팅한다.
-			header: 
-			{
-				packet_id: data.header.packet_id,
-				query_name: data.header.query_name,
-				error_code: 1000
-			}, 
-			body: 
-			{
-				//이곳은 전문에 따라 다르게 응답값을 셋팅해 준다.
-				OutBlock1: 
-				[
-					{
-						'nick_name': '아수쿨',
-						'uid': 1234567,
-						'email': 'asoocool@gmail.com'
-					}
-				]
-			} 
-		};
-	}
-
+	
 	
 
 	res.json(resData);	
