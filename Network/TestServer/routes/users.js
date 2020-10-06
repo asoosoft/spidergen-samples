c45c4aeabf30be8b84fa32355eeace974f741b47
asoosoft <asoosoft7@gmail.com>
2020-10-07 05:07:40 +0900

    ac0003 추가

--- Network/TestServer/routes/users.js
+++ Network/TestServer/routes/users.js
@@ -113,7 +113,31 @@ router.post('/', function(req, res, next)
 			});
 		}
 	}
-	
+    
+    else if(data.header.query_name=='ac003')
+	{
+		resData = 
+		{
+			//클라이언트에서 보낸 헤더값을 그대로 다시 응답데이터에 셋팅한다.
+			header: 
+			{
+				packet_id: data.header.packet_id,
+				query_name: data.header.query_name,
+				error_code: 1000
+			}, 
+			body: 
+			{
+				//이곳은 전문에 따라 다르게 응답값을 셋팅해 준다.
+				OutBlock1: 
+				[
+					{
+						'output1': '아수쿨',
+						'output2': 1234567
+					}
+				]
+			} 
+		};
+	}    
 	
 
 	res.json(resData);	
